faf9892bcdb61d69208c2bd2fb6d4eeb018bca4d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-08-03 06:58:47 +0200

    highligiting the picked tag

--- Random choice picker/app.js
+++ Random choice picker/app.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -47,4 +55,17 @@ function randomSelect() {
       highlightTag(randomTag);
     }, 100);
   }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
